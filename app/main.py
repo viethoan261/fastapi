3c7ac39aefbaecaf9bff18636b47ceb3b024c11f
Nguyễn Việt Hoàn <95644536+viethoan261@users.noreply.github.com>
2025-02-26 22:34:17 +0700

    fix

--- app/main.py
+++ app/main.py
@@ -4,7 +4,7 @@ app = FastAPI()
 
 @app.get("/")
 async def read_root():
-    return {"message": "hahaha!"}
+    return {"message": "TESTTTTTTTTTTTTTTT!"}
 
 @app.get("/hello/{name}")
 async def read_item(name: str):
